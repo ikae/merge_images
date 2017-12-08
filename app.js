fa718312e4a62f68828770d49887e03fe3143bca
ikae <witoon@globe-works.net>
2017-12-08 15:31:55 +0700

    Update variables name

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ const Canvas = require('canvas');
 const fs = require("fs");
 const path = require('path');
 
-const gym_path = 'C:\\Users\\ikae\\Desktop\\Test\\gym\\teams';
-const pokemon_path = 'C:\\Users\\ikae\\Desktop\\Test\\gym\\icons';
+const background_images_path = 'C:\\Users\\ikae\\Desktop\\Test\\gym\\teams';
+const foreground_images_path = 'C:\\Users\\ikae\\Desktop\\Test\\gym\\icons';
 const output_path = 'C:\\Users\\ikae\\Desktop\\Test\\gym\\outputs';
+
+const output_options = {format: 'image/png', width: 96, height: 96};
+const background_offset = { x: 0, y: 0 };
+const foreground_offset = { x: 16, y: 16 };
 
 const pokemonWithImages = [
 	2, 3, 5, 6, 8, 9, 11, 28, 31, 34, 38, 59, 62, 65, 68, 71, 73, 76, 82, 89, 91, 94, 103, 105, 110, 112, 123, 125, 126, 129, 131, 134,
 	135, 136, 137, 139, 143, 144, 145, 146, 150, 153, 156, 159, 243, 244, 245, 248, 249, 250, 302
 ]
 
- function decodeBase64Image(dataString) 
+function decodeBase64Image(dataString) 
 {
   var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
   var response = {};
@@ -28,32 +32,33 @@ const pokemonWithImages = [
   return response;
 }
 
-const gym_arrays = [];
-const pokemon_arrays = [];
+const background_images_array = [];
 
-fs.readdirSync(gym_path).forEach(file => {
-	const file_path = path.join(gym_path, file);
+fs.readdirSync(background_images_path).forEach(file => {
+	const file_path = path.join(background_images_path, file);
 	const file_name = path.basename(file_path, '.png');
-  gym_arrays.push(file_path);
+  background_images_array.push(file_path);
 });
 
-fs.readdirSync(pokemon_path).forEach(file => {
-	const file_path = path.join(pokemon_path, file);
+const foreground_images_array = [];
+
+fs.readdirSync(foreground_images_path).forEach(file => {
+	const file_path = path.join(foreground_images_path, file);
 	const file_name = path.basename(file_path, '.png');
 	if (pokemonWithImages.indexOf(parseInt(file_name))>=0){
-		pokemon_arrays.push(file_path);
+		foreground_images_array.push(file_path);
 	}
 });
 
 const result = [];
-gym_arrays.forEach(gym => {
-	pokemon_arrays.forEach(pokemon => {
-		const raid_name = `${path.basename(gym,'.png')}_${path.basename(pokemon,'.png')}.png`;
-		const raid = path.join(output_path, raid_name);
+background_images_array.forEach(background_file_path => {
+	foreground_images_array.forEach(foreground_file_path => {
+		const output_file_name = `${path.basename(background_file_path,'.png')}_${path.basename(foreground_file_path,'.png')}.png`;
+		const output_file_path = path.join(output_path, output_file_name);
 
-		mergeImages([{ src: gym, x: 0, y: 0 }, { src: pokemon, x: 16, y: 16 }], { Canvas: Canvas, format: 'image/png', width: 96, height: 96 })		
-		.then(b64 => new Promise(function (fulfill, reject){
-			fs.writeFile(raid, decodeBase64Image(b64).data, function (err, res){
+		mergeImages([{ src: background_file_path, x: background_offset.x, y: background_offset.y }, { src: foreground_file_path, x: foreground_offset.x, y: foreground_offset.y }], { Canvas: Canvas, format: output_options.format, width: output_options.width, height: output_options.height })		
+		.then(base64data => new Promise(function (fulfill, reject){
+			fs.writeFile(output_file_path, decodeBase64Image(base64data).data, function (err, res){
 				if (err) reject(err);
 				else fulfill(res);
 			});
